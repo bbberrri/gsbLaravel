3120c66036ac7e4228f15f5a07c68238081d460f
Bilal <bilal@berri.com>
2025-11-16 14:37:02 +0100

    Ajout de commentaires pour plus de clarté

--- app/Http/Controllers/editerFraisController.php
+++ app/Http/Controllers/editerFraisController.php
@@ -10,26 +10,30 @@ class editerFraisController extends Controller
             $gestionnaire = session('gestionnaire');
             $lesVisiteurs = PdoGsb::getLesVisiteursParAnnee();
 
-            $moisA_N = array("01" => "Janvier", "02" => "Février", "03" => "Mars", "04" => "Avril", "05" => "Mai", "06" => "Juin",
+            $mois_string = array("01" => "Janvier", "02" => "Février", "03" => "Mars", "04" => "Avril", "05" => "Mai", "06" => "Juin",
             "07" => "Juillet", "08" => "Août", "09" => "Septembre", "10" => "Octobre", "11" => "Novembre", "12" => "Décembre");
             $lesMois = array();
+            //Boucles permettant d'associer pour un visiteur les mois pour lesquels des frais ont été enregistré
             foreach(array_keys($lesVisiteurs) as $uneAnnee)
             {
                 foreach(array_keys($lesVisiteurs[$uneAnnee]) as $unVisiteur)
                 {
                     $id = $lesVisiteurs[$uneAnnee][$unVisiteur];
-                    $les_mois = PdoGsb::getLesMoisDisponibles($id);
                     $lesMoisVisiteur = array();
+                    $les_mois = PdoGsb::getLesMoisDisponibles($id);
+                    //Boucle remplaçant les numéros des mois envoyés par getLesMoisDisponibles à leurs équivalents en string 
                     foreach($les_mois as $infosMois)
                     {
                         if($infosMois["numAnnee"] == $uneAnnee)
                         {
                             $numMois = $infosMois["numMois"];
-                            array_push($lesMoisVisiteur, $moisA_N[$numMois]);
+                            array_push($lesMoisVisiteur, $mois_string[$numMois]);
                         }
                     }
-
                     $lesMois[$uneAnnee][$unVisiteur] = array("id" => $id, "mois" => $lesMoisVisiteur);
+                    //Les variables $lesVisiteurs et $lesMois sont redondantes 
+                    //$lesVisiteurs est une version tronquée, sans le tableau des mois associés aux visiteurs, de $lesMois
+                    //$lesVisiteurs sert de base à la construction de $lesMois
                 }
             }
 
